1c2ab1bf1c92ab1170853613d9502ee31b56ac21
SergioTx <txitxo37@gmail.com>
2020-05-29 14:12:50 +0200

    207

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,11 +1,13 @@
-const LandingPage = ({ color }) => {
-  console.log('I am in the component', color);
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = () => {
-  console.log('I am on the server');
-  return { color: 'red' };
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser');
+  return response.data;
 };
 
 export default LandingPage;

d10eef54e06c947cbb8f011a0abb01bd1aebfa2b
SergioTx <txitxo37@gmail.com>
2020-06-01 11:57:53 +0200

    231

---
+++ ticketing/common/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var color = {
+    red: 5,
+    green: 15,
+    blue: 25,
+};
+console.log(color);
---
+++ ticketing/common/src/index.ts
@@ -0,0 +1,13 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 5,
+  green: 15,
+  blue: 25,
+};
+
+console.log(color);

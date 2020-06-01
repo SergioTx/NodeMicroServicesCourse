9e87f6a1fdcab3007c4209328e8cf6d0b055d38b
SergioTx <txitxo37@gmail.com>
2020-06-01 08:45:10 +0200

    211

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
+  // console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-  const response = await axios.get('/api/users/currentuser');
-  return response.data;
+  // const response = await axios.get('/api/users/currentuser');
+  // return response.data;
+  console.log('I was executed');
+  return {};
 };
 
 export default LandingPage;

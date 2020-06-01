f8c6df973999877b402d63d78df4929db24f8e19
SergioTx <txitxo37@gmail.com>
2020-06-01 15:49:06 +0200

    254b

--- ticketing/tickets/src/routes/__test__/show.test.ts
+++ ticketing/tickets/src/routes/__test__/show.test.ts
@@ -1,12 +1,15 @@
 import request from 'supertest';
+import mongoose from 'mongoose';
+
 import { app } from '../../app';
 
 it('returns a 404 if the ticket is not found', async () => {
-  const response = await request(app)
-    .get('/api/tickets/kkkkkkkkkkkkkkkkkkkk')
-    .send();
+  const id = new mongoose.Types.ObjectId().toHexString();
 
-  console.log(response.body);
+  const response = await request(app)
+    .get(`/api/tickets/${id}`)
+    .send()
+    .expect(404);
 });
 
 it('returns the ticket if the ticket is found', async () => {

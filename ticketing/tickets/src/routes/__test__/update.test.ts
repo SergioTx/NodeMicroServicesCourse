8fa8a7b11a52f536f47d2682c46f51a008533931
SergioTx <txitxo37@gmail.com>
2020-06-01 16:11:43 +0200

    256

---
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+
+import { app } from '../../app';
+
+it('returns a 404 if the provided ID does not exist', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/ticets/${id}`)
+    .set('Cookie', global.signin())
+    .send({ title: 'Test', price: 20 })
+    .expect(404);
+});
+
+it('returns a 401 if the user is not authenticated', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/ticets/${id}`)
+    .send({ title: 'Test', price: 20 })
+    .expect(401);
+});
+
+it('returns a 401 if the user does not own the ticket', async () => {});
+
+it('returns a 400 if the user provides an invalid title or price', async () => {});
+
+it("updates the ticket's provided valid inputs", async () => {});

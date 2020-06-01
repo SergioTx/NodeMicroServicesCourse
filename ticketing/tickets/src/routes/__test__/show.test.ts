73cf75d0e1609b09d9524ed49543f8be590048e4
SergioTx <txitxo37@gmail.com>
2020-06-01 15:20:36 +0200

    251

---
+++ ticketing/tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 404 if the ticket is not found', async () => {
+  await request(app)
+    .get('/api/tickets/kkkkkkkkkkkkkkkkkkkk')
+    .set('Cookie', global.signin())
+    .send()
+    .expect(404);
+});
+
+it('returns the ticket if the ticket is found', async () => {
+  const title = 'TestTitle';
+  const price = 20;
+
+  const response = await request(app)
+    .get('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price,
+    })
+    .expect(201);
+
+  const ticketResponse = await request(app)
+    .get(`api/tickets/${response.body.id}`)
+    .send()
+    .expect(200);
+
+  expect(ticketResponse.body.title).toEqual(title);
+  expect(ticketResponse.body.price).toEqual(price);
+});

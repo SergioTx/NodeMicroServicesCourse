5e698a96b1c5fe4c32250848fe7d1e4360c75ba8
SergioTx <txitxo37@gmail.com>
2020-06-01 09:17:16 +0200

    215

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,29 +1,13 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  // const response = await axios.get('/api/users/currentuser');
-  // return response.data;
-  if (typeof window === 'undefined') {
-    // we are on the server
-    // request should be made to http://ingress-nginx.../
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    // we are on the browser
-    // request can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;

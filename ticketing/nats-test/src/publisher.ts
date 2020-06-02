85ad00926eb799dc2f58da3208b282b08f84ac7d
SergioTx <txitxo37@gmail.com>
2020-06-02 16:57:25 +0200

    294

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -8,22 +8,17 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  // const data = JSON.stringify({
-  //   id: '123',
-  //   title: 'concert',
-  //   price: 20,
-  // });
-
-  // stan.publish('ticket:created', data, () => {
-  //   console.log('Event published');
-  // });
-
-  new TicketCreatedPublisher(stan).publish({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });

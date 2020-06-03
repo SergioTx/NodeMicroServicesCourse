193a6c9c97ed1d27f96c9148dbb35ecf00a99df6
SergioTx <txitxo37@gmail.com>
2020-06-03 17:16:43 +0200

    328

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@stxtickets/common';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+} from '@stxtickets/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -18,6 +24,21 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure the ticket is not already reserved
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that the order was created
+
     res.send();
   }
 );

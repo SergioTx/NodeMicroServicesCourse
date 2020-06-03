2d6027a82b6162e3eb97e942ca35fadd604ba7e6
SergioTx <txitxo37@gmail.com>
2020-06-03 12:22:56 +0200

    314

--- ticketing/tickets/src/routes/__test__/update.test.ts
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 
 import { app } from '../../app';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided ID does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -76,4 +77,23 @@ it("updates the ticket's provided valid inputs", async () => {
 
   expect(ticketResponse.body.title).toEqual(title);
   expect(ticketResponse.body.price).toEqual(price);
+});
+
+it('publishes an event', async () => {
+  const title = 'NewTitle';
+  const price = 100;
+
+  const cookie = global.signin();
+  const response = await request(app)
+    .post('/api/tickets/')
+    .set('Cookie', cookie)
+    .send({ title: 'FirstTest', price: 20 });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({ title, price })
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });

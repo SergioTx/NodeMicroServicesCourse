f0fb4378a898d76c5e7639ac032d21c16f6994aa
SergioTx <txitxo37@gmail.com>
2020-06-03 09:41:50 +0200

    304

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -4,6 +4,7 @@ import { requireAuth, validateRequest } from '@stxtickets/common';
 
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -22,7 +23,7 @@ router.post(
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
 
-    new TicketCreatedPublisher(client).publish({
+    new TicketCreatedPublisher(natsWrapper.client).publish({
       // need to take it from ticket as it can internally change
       id: ticket.id,
       title: ticket.title,

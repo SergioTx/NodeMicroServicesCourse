b2ed35ef9f0fac756f0df3dbeac143e2bff1258b
SergioTx <txitxo37@gmail.com>
2020-06-04 10:07:10 +0200

    340

--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -1,9 +1,34 @@
 import express, { Request, Response } from 'express';
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from '@stxtickets/common';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send();
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // 204 is deleted when it is really only cancelled
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };

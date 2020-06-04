e558311f31898c2c56b1bd558ba2c876905245db
SergioTx <txitxo37@gmail.com>
2020-06-04 12:43:31 +0200

    346

--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -5,6 +5,8 @@ import {
   NotAuthorizedError,
 } from '@stxtickets/common';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -14,7 +16,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -26,7 +28,13 @@ router.delete(
     order.status = OrderStatus.Cancelled;
     await order.save();
 
-    // TODO publishing an event saying this was cancelled!
+    // publish an event saying this was cancelled!
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     // 204 is deleted when it is really only cancelled
     res.status(204).send(order);

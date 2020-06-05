1dddb4ab105d124a26fd0e60903925bb671ce070
SergioTx <txitxo37@gmail.com>
2020-06-05 08:53:34 +0200

    369

--- ticketing/orders/src/models/ticket.ts
+++ ticketing/orders/src/models/ticket.ts
@@ -17,6 +17,13 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  /**
+   * Finds the element with the ID and the previous version
+   */
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -43,6 +50,13 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.set('versionKey', 'version');
 ticketSchema.plugin(updateIfCurrentPlugin);
+
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({

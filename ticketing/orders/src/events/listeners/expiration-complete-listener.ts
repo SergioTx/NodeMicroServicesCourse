2c6d64b1cd640e8a433e94ae09f9c3d852b39458
SergioTx <txitxo37@gmail.com>
2020-06-10 15:21:22 +0200

    406

---
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,28 @@
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  OrderStatus,
+  Subjects,
+} from '@stxtickets/common';
+
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+import { queueGroupName } from './queue-group-name';
+
+export class ExpirationCompleteListener extends Listener<
+  ExpirationCompleteEvent
+> {
+  readonly subject = Subjects.ExpirationComplete;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({ status: OrderStatus.Cancelled, ticket: null });
+  }
+}

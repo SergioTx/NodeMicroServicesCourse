ca505dcb78f9b33f3880b19e06c176cf2386e0eb
SergioTx <txitxo37@gmail.com>
2020-06-10 15:31:49 +0200

    407

--- ticketing/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -7,6 +7,8 @@ import {
 
 import { Message } from 'node-nats-streaming';
 import { Order } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+import { natsWrapper } from '../../nats-wrapper';
 import { queueGroupName } from './queue-group-name';
 
 export class ExpirationCompleteListener extends Listener<
@@ -17,12 +19,23 @@ export class ExpirationCompleteListener extends Listener<
   queueGroupName = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate('ticket');
 
     if (!order) {
       throw new Error('Order not found');
     }
 
-    order.set({ status: OrderStatus.Cancelled, ticket: null });
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+
+    await new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
   }
 }
